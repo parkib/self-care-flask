ec27427a877fbb43f59a2596c74835fb9223075e
parkib <prakrutib2008@gmail.com>
2024-05-28 11:06:47 -0700

    quotes!!

--- api/quote.py
+++ api/quote.py
@@ -5,14 +5,14 @@ from datetime import datetime
 from auth_middleware import token_required
 from flask_cors import CORS
 
+
 from model.quotes import Quote
 
-quote_api = Blueprint('quote_api', __name__,
-                   url_prefix='/api/quotes')
 
-# API docs https://flask-restful.readthedocs.io/en/latest/api.html
+quote_api = Blueprint('quote_api', __name__, url_prefix='/api/quotes')
 api = Api(quote_api)
 CORS(quote_api)
+
 
 class QuoteAPI:
     class _CRUD(Resource):
@@ -21,16 +21,19 @@ class QuoteAPI:
             quotename = body.get('quotename')
             quoteauthor = body.get('quoteauthor')
             opinion = body.get('opinion')
-            quote = Quote(quotename=quotename, quoteauthor=quoteauthor, opinion=opinion)
+            rating = body.get('rating')
+            quote = Quote(quotename=quotename, quoteauthor=quoteauthor, opinion=opinion, rating=rating)
             quote = quote.create()
             if quote:
                 return quote.read()
             return {'message': f'Processed {quotename}, either a format error or '}, 400
 
+
         def get(self):
             quotes = Quote.query.all()
             json_ready = [quote.read() for quote in quotes]
             return jsonify(json_ready)
+
 
     class _Create(Resource):
         def post(self):
@@ -38,29 +41,27 @@ class QuoteAPI:
             quotename = body.get('quotename')
             quoteauthor = body.get('quoteauthor')
             opinion = body.get('opinion')
-            new_quote = Quote(quotename=quotename, quoteauthor=quoteauthor, opinion=opinion)
+            rating = body.get('rating')
+            new_quote = Quote(quotename=quotename, quoteauthor=quoteauthor, opinion=opinion, rating=rating)
             quote = new_quote.create()
             return jsonify(quote.read())
-            # return jsonify({
-            #     "id": 1,
-            #     "quotename": "c1",
-            #     "quoteauthor": "c1",
-            #     "opinion": "c1"
-            #     })
+
 
     class _Read(Resource):
         def get(self):
             quotes = Quote.query.all()
             json_ready = [quote.read() for quote in quotes]
             return jsonify(json_ready)
-            # return jsonify({
-            #     "id": 1,
-            #     "quotename": "c1",
-            #     "quoteauthor": "c1",
-            #     "opinion": "c1"
-            #     })
 
-    # building RESTapi endpoint
+
+    class _FilterByRating(Resource):
+        def get(self, rating):
+            quotes = Quote.query.filter_by(rating=rating).all()
+            json_ready = [quote.read() for quote in quotes]
+            return jsonify(json_ready)
+
+
     api.add_resource(_CRUD, '/')
     api.add_resource(_Read, '/read')
     api.add_resource(_Create, '/make')
+    api.add_resource(_FilterByRating, '/filter/<int:rating>')
--- model/quotes.py
+++ model/quotes.py
@@ -1,91 +1,104 @@
-""" database dependencies to support sqliteDB examples """
 from random import randrange
 from datetime import date
 import os, base64
 import json
+
 
 from __init__ import app, db
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-
 class Quote(db.Model):
-    __tablename__ = 'quotes'  # table name is plural, class name is singular
-    # Define the User schema with "vars" from object
+    __tablename__ = 'quotes'
     id = db.Column(db.Integer, primary_key=True)
     _quotename = db.Column(db.String(255), unique=False, nullable=False)
     _quoteauthor = db.Column(db.String(255), unique=False, nullable=False)
     _opinion = db.Column(db.String(255), unique=False, nullable=False)
-    # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
-    # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, quotename, quoteauthor, opinion):
-        self._quotename = quotename    # variables with self prefix become part of the object,
+    _rating = db.Column(db.Integer, nullable=False)
+
+
+    def __init__(self, quotename, quoteauthor, opinion, rating):
+        self._quotename = quotename
         self._quoteauthor = quoteauthor
         self._opinion = opinion
-   
+        self._rating = rating
+
+
     @property
     def quotename(self):
         return self._quotename
+
+
     @quotename.setter
     def quotename(self, quotename):
         self._quotename = quotename
-         
+
+
     @property
     def quoteauthor(self):
         return self._quoteauthor
+
+
     @quoteauthor.setter
     def quoteauthor(self, quoteauthor):
         self._quoteauthor = quoteauthor
 
+
     @property
     def opinion(self):
         return self._opinion
+
+
     @opinion.setter
     def opinion(self, opinion):
         self._opinion = opinion
-        
+
+
+    @property
+    def rating(self):
+        return self._rating
+
+
+    @rating.setter
+    def rating(self, rating):
+        self._rating = rating
+
+
     def __str__(self):
         return json.dumps(self.read())
 
 
     def create(self):
         try:
-            # creates a person object from User(db.Model) class, passes initializers
-            db.session.add(self)  # add prepares to persist person object to Users table
-            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
+            db.session.add(self)
+            db.session.commit()
             return self
         except IntegrityError:
             db.session.remove()
             return None
-    
+
 
     def read(self):
         return {
             "id": self.id,
             "quotename": self.quotename,
             "quoteauthor": self.quoteauthor,
-            "opinion": self.opinion,            
+            "opinion": self.opinion,
+            "rating": self.rating
         }
-    
 
-"""Database Creation and Testing """
-# Builds working data for testing
+
 def initQuotes():
     with app.app_context():
-        """Create database and tables"""
         db.create_all()
-        """Tester data for table"""
         quotes = [
-            Quote(quotename="Carpe Diem", quoteauthor="Horous Magic", opinion="Quote is very great!"),
-            Quote(quotename="It is a great day", quoteauthor="Albert Dias", opinion="I know it is a great day")
+            Quote(quotename="Carpe Diem", quoteauthor="Horous Magic", opinion="Quote is very great!", rating=5),
+            Quote(quotename="It is a great day", quoteauthor="Albert Dias", opinion="I know it is a great day", rating=4)
         ]
-        """Builds sample user/note(s) data"""
         for quote in quotes:
             try:
                 quote.create()
             except IntegrityError:
-                '''fails with bad or duplicate data'''
                 db.session.remove()
                 print(f"Records exist, duplicate email, or error: {quote.quoteauthor}")
-            
